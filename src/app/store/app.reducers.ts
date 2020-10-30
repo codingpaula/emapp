1c3fbdc6cc54bae21fb6bf58534298ca8a52907e
Manuela Paula Ritter <m.ritter@slashwhy.de>
2020-10-30 17:11:06 +0100

    turn on typescript strict and fix stuff

--- src/app/store/app.reducers.ts
+++ src/app/store/app.reducers.ts
@@ -1,5 +1,7 @@
+import { ActionReducerMap } from '@ngrx/store';
 import { matrixReducer } from '../matrix/matrix.reducers';
+import { AppState } from './app.state';
 
-export const appReducers = {
+export const appReducers: ActionReducerMap<AppState> = {
   matrix: matrixReducer,
 };
