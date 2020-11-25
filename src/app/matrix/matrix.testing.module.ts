5cefed3f81bf694df0e78654fb8ae7a478a33006
Manuela Paula Ritter <m.ritter@slashwhy.de>
2020-11-25 11:43:07 +0100

    add matrix testing module for mock classes

---
+++ src/app/matrix/matrix.testing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CanvasMockComponent } from './canvas/canvas.component.mock';
+import { DetailMockComponent } from './detail/detail.component.mock';
+import { SidebarMockComponent } from './sidebar/sidebar.component.mock';
+import { TaskDotMockComponent } from './task-dot/task-dot.component.mock';
+import { TopicButtonMockComponent } from './topic-button/topic-button.component.mock';
+import { XPositionMockPipe } from './x-position.pipe.mock';
+import { YPositionMockPipe } from './y-position.pipe.mock';
+
+@NgModule({
+  declarations: [
+    CanvasMockComponent,
+    DetailMockComponent,
+    SidebarMockComponent,
+    TaskDotMockComponent,
+    TopicButtonMockComponent,
+    XPositionMockPipe,
+    YPositionMockPipe,
+  ],
+  exports: [
+    CanvasMockComponent,
+    DetailMockComponent,
+    SidebarMockComponent,
+    TaskDotMockComponent,
+    TopicButtonMockComponent,
+    XPositionMockPipe,
+    YPositionMockPipe,
+  ],
+})
+export class MatrixTestingModule {}
