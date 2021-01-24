07a5e2c31285c8a58376eb6e0bad1750ad640fc0
Manuela Paula Ritter <m.ritter@slashwhy.de>
2021-01-24 12:38:54 +0100

    add unsubscribe to subscriptions in detail

--- src/app/matrix/detail/detail.component.ts
+++ src/app/matrix/detail/detail.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
 import { Task, TopicDictionary } from '../matrix.interfaces';
 import { MatrixService } from '../matrix.service';
 
@@ -8,15 +9,22 @@ import { MatrixService } from '../matrix.service';
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.scss'],
 })
-export class DetailComponent implements OnInit {
+export class DetailComponent implements OnInit, OnDestroy {
   taskHistory: Task[] = [];
   topics: TopicDictionary = {};
+
+  unsubscribe$ = new Subject<void>();
 
   constructor(private readonly matrixService: MatrixService) {}
 
   ngOnInit(): void {
     this.subscribeToTaskHistory();
     this.subscribeToTopics();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   changeTask(task: Task): void {
@@ -35,6 +43,7 @@ export class DetailComponent implements OnInit {
     this.matrixService
       .selectCurrentTaskHistory()
       .pipe(
+        takeUntil(this.unsubscribe$),
         map((tasks) => {
           if (tasks) {
             this.taskHistory = tasks;
@@ -48,6 +57,7 @@ export class DetailComponent implements OnInit {
     this.matrixService
       .selectTopics()
       .pipe(
+        takeUntil(this.unsubscribe$),
         map((topics) => {
           if (topics.length > 0) {
             topics.forEach((t) => (this.topics[t.id] = t));
